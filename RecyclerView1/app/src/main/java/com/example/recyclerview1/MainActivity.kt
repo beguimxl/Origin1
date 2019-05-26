54331a3f0710e3203c731e12157694a57fd9ee40
Bilguun.Soninbayar <banthehooks@yahoo.com>
2019-05-26 19:12:37 +0800

    First Commit

---
+++ RecyclerView1/app/src/main/java/com/example/recyclerview1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.recyclerview1
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+             //   message.setText(R.string.title_home)
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.mainLayout, SuperHeroesFragment())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+             //   message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+               // message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
